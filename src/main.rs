776c909d683e258097a8806a1222be634fef6783
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2025-10-15 14:38:40 -0400

    Update Basic Server Example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+
+#[tokio::main]
+async fn main() {
+    let app: Router<()> = Router::new()
+        .route("/", get(root))
+        .route("/foo", get(get_foo).post(post_foo))
+        .route("/foo/bar", get(foo_bar));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+
+// which calls one of these handlers
+async fn root() -> String {
+    String::from("hello axum")
+}
+async fn get_foo() -> String {
+    String::from("get请求的foo")
+}
+async fn post_foo() -> String {
+    String::from("post请求的foo")
+}
+async fn foo_bar() -> String {
+    String::from("foo:bar")
 }
